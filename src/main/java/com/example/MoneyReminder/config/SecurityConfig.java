db21ea35752caabb607e18623ca231678259c59e
Becksta <christina.beck@student.htw-berlin.de - ->
2021-06-02 08:18:34 +0200

    okta properties und SecurityConfig geändert

---
+++ src/main/java/com/example/MoneyReminder/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.example.MoneyReminder.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                // public pages
+                .antMatchers(
+                        HttpMethod.GET,
+                        Endpoints.Site.CREAT_FRIENDS,
+                        Endpoints.Site.REMINDER,
+                        Endpoints.Site.NOTES,
+                        Endpoints.Site.CONTACT_LIST
+                ).permitAll()
+                // static resources
+                .antMatchers(
+                        "/css/**",
+                        "/images/**",
+                        "/js/**"
+                ).permitAll()
+                .anyRequest().authenticated()
+
+                // send the user back to the root page when they logout
+                .and()
+                .logout().logoutSuccessUrl("/?logout")
+
+                .and()
+                .oauth2Client()
+                .and()
+                .oauth2Login();
+    }
+}
